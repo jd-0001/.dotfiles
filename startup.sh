27c6f8a3cdd152f7262636f3f1fc8a9966a4d862
jd-0001 <jdsolanki0001@gmail.com>
2022-04-14 03:18:43 +0530

    improved

--- startup.sh
+++ startup.sh
@@ -1,10 +1,24 @@
+set -e
+
+# install curl
 sudo apt install curl
 
 # Snaps
 echo "Let's remove snaps now :)"
 echo "Below is list of snaps"
 snap list
-echo 'Remove listed snaps using "sudo snap remove --purge <package-name>"'
+echo "Removing possible snaps..."
+sudo snap remove --purge firefox
+sudo snap remove --purge gnome-3-38-2004
+sudo snap remove --purge snapd-desktop-integration
+sudo snap remove --purge snap-store
+sudo snap remove --purge gtk-common-themes
+sudo snap remove --purge bare
+sudo snap remove --purge core20
+sudo snap remove --purge snapd
+echo "Remaining snaps"
+snap list
+echo 'Remove listed snaps using "sudo snap remove --purge <package-name>" in separate tab'
 echo 'Press key when done removing all packages'
 read is_snaps_removed
 sudo rm -rf /var/cache/snapd/
