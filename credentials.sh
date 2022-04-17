2a76bd6487ed6fef5ab91561b871ad3a6f4f377c
jd-0001 <jdsolanki0001@gmail.com>
2022-04-17 09:02:49 +0530

    asdf

--- credentials.sh
+++ credentials.sh
@@ -5,7 +5,7 @@ dotfiles_cred_dir="/tmp/.dotfiles"
 
 infoh1 "Credentials"
 
-pblue "Now, we need zip `dotfiles*.zip` in /tmp dir and all files that should be placed in \$HOME dir should be in \"/tmp/.dotfiles/\$HOME\" dir"
+pblue "Now, we need zip \"dotfiles*.zip\" in /tmp dir and all files that should be placed in \$HOME dir should be in \"/tmp/.dotfiles/\$HOME\" dir"
 
 pblue "Download your .dotfiles zip that contains your credentials files and make sure it's in /tmp dir"
 wait_for_keypress
@@ -16,6 +16,7 @@ unzip /tmp/dotfiles*.zip -d /tmp
 HOME_files="$dotfiles_cred_dir/\$HOME"
 pblue "Copying files from $HOME_files"
 if [[ -d $HOME_files ]]; then
-    cp $HOME_files/* ~
+    cp -r $HOME_files/* ~
 else
     echo -e "Directory doesn't exist: $HOME_files"
+fi
