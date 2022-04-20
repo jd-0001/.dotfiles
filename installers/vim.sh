af4afb1483e0bbe1427abf70fcaa913d9f336266
jd-0001 <jdsolanki0001@gmail.com>
2022-04-20 23:59:48 +0530

    asd

--- installers/vim.sh
+++ installers/vim.sh
@@ -10,3 +10,9 @@ pgreen "vim gui installed\n"
 pblue "Installing vim runtime"
 sudo apt-get install vim-runtime -y >> "$LOG_DIR.dotfiles.log"
 pgreen "vim runtime installed\n"
+
+pblue "Installing nginx plugin"
+mkdir -p ~/.vim/pack/plugins/start
+cd ~/.vim/pack/plugins/start
+git clone https://github.com/chr4/nginx.vim.git
+pgreen "Nginx plugin installation processed\n"
