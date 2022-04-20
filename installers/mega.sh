ca440b225926efb4c2c6106e524e4d1967cfd07f
JD Solanki <jdsolanki0001@gmail.com>
2022-04-20 09:48:01 +0530

    asd

--- installers/mega.sh
+++ installers/mega.sh
@@ -6,11 +6,12 @@ infoh1 "Mega"
 pblue "It assumes you are using ubuntu. Press any key to continue."
 wait_for_keypress
 
-ubuntu_version=lsb_release -r | grep -oP "[\d\.]+"
+ubuntu_version=$(lsb_release -r | grep -oP "[\d\.]+")
 
 # Download mega
 pblue "Downloading megasync & nautils integration from available files..."
 echo "Press any key once both downloads are done"
+echo -e "https://mega.nz/linux/repo/xUbuntu_$ubuntu_version/amd64/"
 pblue "Opening browser in 3 seconds..."
 sleep 3
 google-chrome "https://mega.nz/linux/repo/xUbuntu_$ubuntu_version/amd64/" > /dev/null 2>&1 &
@@ -18,7 +19,8 @@ wait_for_keypress
 
 # Install mega
 pblue "Installing mega sync now..."
-sudo gdebi /tmp/megasync*.deb >> "$LOG_DIR.dotfiles.log"
+# sudo gdebi /tmp/megasync*.deb >> "$LOG_DIR.dotfiles.log"
+sudo gdebi /tmp/megasync*.deb
 pgreen "mega sync installed\n"
 
 # WIP
