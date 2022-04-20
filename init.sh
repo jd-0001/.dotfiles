ddd75218d121de492c97243470396d9f18050caf
jd-0001 <jdsolanki0001@gmail.com>
2022-04-20 23:43:50 +0530

    sd

--- init.sh
+++ init.sh
@@ -72,8 +72,8 @@ source "$(dirname "$0")/utils/vars.sh"
 # /bin/bash "$(dirname "$0")/installers/poetry.sh"
 # /bin/bash "$(dirname "$0")/installers/pyenv.sh"
 # /bin/bash "$(dirname "$0")/installers/secret-tool.sh"
-/bin/bash "$(dirname "$0")/installers/dracula-theme.sh"
-# /bin/bash "$(dirname "$0")/installers/xonsh.sh"
+# /bin/bash "$(dirname "$0")/installers/dracula-theme.sh"
+/bin/bash "$(dirname "$0")/installers/xonsh.sh"
 
 # symlinks
 # /bin/bash "$(dirname "$0")/link.sh"
