aa05eda5c955afbc96fa0580d2e66bf8620e9605
jd-0001 <jdsolanki0001@gmail.com>
2022-04-20 23:53:56 +0530

    sdf

--- installers/xonsh.sh
+++ installers/xonsh.sh
@@ -20,37 +20,37 @@ pblue "setting xonsh as default shell..."
 chsh -s $(which xonsh)
 echo -e "\n\n"
 
-# infoh1 "xonsh extensions"
+infoh1 "xonsh extensions"
 
-# # https://github.com/oh-my-xonsh/xontrib-up
-# pblue "Installing up"
-# /usr/bin/xonsh -c "xpip install xontrib-up" >> "$LOG_DIR.dotfiles.log"
-# echo "xontrib load up" >> ~/.xonshrc
-# echo -e "\n"
+# https://github.com/oh-my-xonsh/xontrib-up
+pblue "Installing up"
+/usr/bin/xonsh -c "xpip install xontrib-up" >> "$LOG_DIR.dotfiles.log"
+echo "xontrib load up" >> ~/.xonshrc
+echo -e "\n"
 
-# # https://github.com/jnoortheen/xontrib-cmd-durations
-# pblue "Installing cmd_done"
-# /usr/bin/xonsh -c "xpip install xontrib-cmd-durations" >> "$LOG_DIR.dotfiles.log"
-# echo "xontrib load cmd_done" >> ~/.xonshrc
-# echo -e "\n"
+# https://github.com/jnoortheen/xontrib-cmd-durations
+pblue "Installing cmd_done"
+/usr/bin/xonsh -c "xpip install xontrib-cmd-durations" >> "$LOG_DIR.dotfiles.log"
+echo "xontrib load cmd_done" >> ~/.xonshrc
+echo -e "\n"
 
-# # https://github.com/oh-my-xonsh/xontrib-default-command
-# pblue "default command"
-# /usr/bin/xonsh -c "xpip install xontrib-default-command" >> "$LOG_DIR.dotfiles.log"
-# echo "xontrib load default_command" >> ~/.xonshrc
-# echo -e "\n"
+# https://github.com/oh-my-xonsh/xontrib-default-command
+pblue "default command"
+/usr/bin/xonsh -c "xpip install xontrib-default-command" >> "$LOG_DIR.dotfiles.log"
+echo "xontrib load default_command" >> ~/.xonshrc
+echo -e "\n"
 
-# # https://github.com/cafehaine/xontrib-xlsd
-# pblue "xlsd"
-# /usr/bin/xonsh -c "xpip install xontrib-xlsd" >> "$LOG_DIR.dotfiles.log"
-# echo "xontrib load xlsd" >> ~/.xonshrc
-# echo -e "\n"
+# https://github.com/cafehaine/xontrib-xlsd
+pblue "xlsd"
+/usr/bin/xonsh -c "xpip install xontrib-xlsd" >> "$LOG_DIR.dotfiles.log"
+echo "xontrib load xlsd" >> ~/.xonshrc
+echo -e "\n"
 
-# # https://github.com/dyuri/xontrib-langenv
-# pblue "xontrib-langenv - pyenv"
-# /usr/bin/xonsh -c "xpip install xontrib-langenv" >> "$LOG_DIR.dotfiles.log"
-# echo "xontrib load pyenv" >> ~/.xonshrc
-# echo -e "\n"
+# https://github.com/dyuri/xontrib-langenv
+pblue "xontrib-langenv - pyenv"
+/usr/bin/xonsh -c "xpip install xontrib-langenv" >> "$LOG_DIR.dotfiles.log"
+echo "xontrib load pyenv" >> ~/.xonshrc
+echo -e "\n"
 
 pblue "You need to login and logout to use xonsh as default shell"
 pgreen "xonsh setup completed"
