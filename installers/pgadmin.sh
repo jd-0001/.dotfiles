db869cbd930c0318f5e23f2403915adb1089d245
JD Solanki <jdsolanki0001@gmail.com>
2022-04-25 11:04:25 +0530

    asd

---
+++ installers/pgadmin.sh
@@ -0,0 +1,21 @@
+source "$(dirname "$0")/../utils/functions.sh"
+source "$(dirname "$0")/../utils/vars.sh"
+
+infoh1 "pgadmin4"
+
+sudo mkdir /var/lib/pgadmin
+sudo mkdir /var/log/pgadmin
+sudo chown $USER /var/lib/pgadmin
+sudo chown $USER /var/log/pgadmin
+
+echo -e "Creating new virtual environment..."
+python3 -m venv pgadmin4
+source pgadmin4/bin/activate
+
+echo -e "Installing pgadmin4"
+pip install pgadmin4
+pgreen "pgadmin4 installed successfully\n"
+
+echo -e "Let's configure pgadmin4 with email: user@domain.com & password: adminadmin\n"
+echo -e "Press control+c to exit once configured and pgadmin4 server ran"
+pgadmin4
